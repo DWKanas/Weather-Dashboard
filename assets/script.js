580ea9afd11a4ddc45b5ae47ff076a7850daa61a
DWKanas <dkanas0122@gmail.com>
2022-06-16 20:50:04 -0700

    made variables to grab elements from html

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,11 @@
-console.log("test")
+//Grabbing all elements from html
+const inputEl = document.getElementById("city-input");
+const searchEl = document.getElementById("search-button");
+const clearEl = document.getElementById("clear-history");
+const nameEl = document.getElementById("city-name");
+const currentPicEl = document.getElementById("current-pic");
+const currentTempEl = document.getElementById("temperature");
+const currentHumidityEl = document.getElementById("humidity");
+const currentWindEl = document.getElementById("wind-speed");
+const currentUVEl = document.getElementById("UV-index");
+const historyEl = document.getElementById("history");
